4ad564f34c6e19f853f6b74748997eb50564b58c
GeiselJoselin <geiselrf@gmail.com>
2023-12-21 17:02:00 -0600

    Added GoogleBooksAPI

---
+++ BookFinder/Models/Constant.swift
@@ -0,0 +1,13 @@
+//
+//  Constant.swift
+//  BookFinder
+//
+//  Created by Geisel Roque on 21/12/23.
+//
+
+import Foundation
+
+struct Constant {
+  static let baseURL = "https://www.googleapis.com/books/v1/volumes"
+  static let apiKey = "AIzaSyCOltFW9O7GZoHBMXZd_UdFvoM1BxAHXoA"
+}
---
+++ BookFinder/Models/VolumeData.swift
@@ -0,0 +1,13 @@
+//
+//  VolumeData.swift
+//  BookFinder
+//
+//  Created by Geisel Roque on 21/12/23.
+//
+
+import Foundation
+
+struct VolumeData: Codable {
+    let title: String
+    let authors: [String]
+}
